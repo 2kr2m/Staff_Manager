53726552777eb84ac4cdc2b2652afd88bb983c90
2kr2m <ak1ram@outlook.com>
2022-11-16 15:55:12 +0100

    Staff_Manager

---
+++ springboot-backend/src/main/java/com/kr2m/javaproject/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.kr2m.javaproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kr2m.javaproject.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long > {
+
+}
